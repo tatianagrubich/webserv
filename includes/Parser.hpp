6ebf6fd41e79feac2bce370295e793aa0997cc76
Louanne Fornio <lfornio@pr-m5.kzn.21-school.ru>
2022-06-01 20:04:23 +0300

    parser ok

--- includes/Parser.hpp
+++ includes/Parser.hpp
@@ -1,10 +1,10 @@
 #ifndef PARSER_HPP
-# define PARSER_HPP
-# include <iostream>
-# include <vector>
-# include <fstream>
-# include "InfoServer.hpp"
-# include "Location.hpp"
+#define PARSER_HPP
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "InfoServer.hpp"
+#include "Location.hpp"
 
 #define GREEN "\033[32m"
 #define BLUE "\033[34m"
@@ -13,19 +13,14 @@
 
 class Parser
 {
-	std::string config_file_name;
 	std::vector<InfoServer> servers;
-	std::ifstream fin;
 	std::vector<std::string> file;
 	int index;
-
-
-
+	std::string config_file_name;
+	std::ifstream fin;
 	std::string string_from_config_file;
-	// static const int	bits = 8;
 
 	void errorConfigFile();
-	// void analysisStringFromConfigFile();
 	void createInfoServer();
 	bool createLocation(InfoServer &info_server);
 	void readConfigFile();
@@ -33,10 +28,6 @@ class Parser
 
 public:
 	Parser(char *config_file_name);
-	// ~InfoServer(void);
-	// InfoServer(const InfoServer &other);
-	// InfoServer	&operator=(const InfoServer &other);
-
 	std::vector<InfoServer> parserConfigFile();
 	void printServers();
 };
